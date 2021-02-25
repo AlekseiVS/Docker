cbdc8d53405a40c2923b00cde2fe58a0c9d5551f
a.sokoliuk <a.sokoliuk@devcaz.com>
2021-02-25 10:08:44 +0200

    clean

--- index.php
+++ index.php
@@ -3,14 +3,6 @@ require_once ('vendor/autoload.php');
 
 use App\src\Db;
 use App\src\Params;
-use App\src\Hello;
-use App\src\test\Test;
-
-$hello = new Hello();
-$hello('Hello!');
-
-$test = new Test();
-$test->getClassName();
 
 $res = Db::getInstance(Params::getDbParams())->executeQuery("SELECT email FROM user");
 
